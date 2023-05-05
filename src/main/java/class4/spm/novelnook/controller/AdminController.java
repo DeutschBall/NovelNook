61e4b2f662ca6f69a1e6d790c3a88c72c49dc076
jxxyh <128386560+jxxyh@users.noreply.github.com>
2023-05-05 22:17:21 +0800

    Update AdminController.java

--- src/main/java/class4/spm/novelnook/controller/AdminController.java
+++ src/main/java/class4/spm/novelnook/controller/AdminController.java
@@ -76,12 +76,11 @@ public class AdminController {
         //flag is used to judge whether operation is success
         int flag = adminMapper.updateByUserName(staff);
 
-        if(flag > 0) {
+        if (flag > 0) {
             return R.success(null);
         }
 
         return R.error("update fail");
-
     }
 
 //    测试用的登录界面在/resources/static/admin/log.html,访问url：localhost:8080/admin/login
@@ -158,6 +157,57 @@ public class AdminController {
             return R.success(avatar);
         }
     }
+    
+    /**
+     * Add new staff function
+     * @param staff param got from json data in RequestBody
+     * @return
+     */
+    @PostMapping("/staff")
+    public R addNewStaff(@RequestBody Staff staff) {
 
+        int flag = adminMapper.addNewStaff(staff);
+
+        if (flag > 0) {
+            return R.success(null);
+        }
+
+        return R.error("Add new staff fail.\n" + "This staff maybe exist.");
+    }
+    
+    /**
+     * User can upload their own pictures as their avatar
+     * @param file   pictures upload by user
+     * @return
+     */
+    @PostMapping("/uploadAvatar")
+    public R uploadAvatar(MultipartFile file){
+
+        //judge whether file is null
+        if (file.isEmpty()) {
+            return R.error("file not exist or file type error");
+        }
+
+        //rename file
+        String originalFilename = file.getOriginalFilename();
+        //get suffix: .png, .jpg, etc;
+        String suffix = "." + originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+        //create a random file name
+        String uuid = UUID.randomUUID().toString().replace("-","");
+
+        //upload file
+        ApplicationHome applicationHome = new ApplicationHome(this.getClass());
+        String pre = applicationHome.getDir().getParentFile().getParentFile().getAbsolutePath()
+                + "\\src\\main\\resources\\static\\avatars\\";
+        String path = pre + uuid + suffix;
+
+        try {
+            file.transferTo(new File(path));
+            return R.success(path);     //send path to frontend
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return R.error("upload avatar fail");
+    }
 
 }
