f1a41456ded5771f51497338f7c260c6606c2148
jxxyh <128386560+jxxyh@users.noreply.github.com>
2023-05-07 16:13:45 +0800

    Add files via upload

---
+++ src/main/java/class4/spm/novelnook/pojo/Fine.java
@@ -0,0 +1,15 @@
+package myorg.domain;
+//package class4.spm.novelnook.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//罚款金额
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Fine {
+    double money;
+}
