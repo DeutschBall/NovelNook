0448b23d090138d7ebfc66d62adc75ccc9c773e8
ChildishChen <25111154@qq.com>
2023-05-09 23:20:32 +0800

    add a dropdown to log out

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,8 +2,16 @@
   <div id="app">
     <div style="height: 60px;line-height: 60px;background-color: white; display: flex; justify-content: flex-start; align-items: center;">
       <img src="@/assets/logo.png" style="height: 60px">
-      <span v-if="userid===null" style="margin-left: auto; font-family: Arial;">Please login {{ userid }}</span>
-      <span v-else style="margin-left: auto; font-family: Arial;">User ID: {{ userid }}</span>
+      <el-dropdown @command="handleCommand" style="margin-left: auto;">
+        <span class="el-dropdown-link" style="margin-left: 20px;">
+            <span style="font-family: Arial;">{{this.userid===NULL ? 'Please login':'User Id: '+this.userid}}<i v-if="userid!==NULL" class="el-icon-arrow-down el-icon--right"></i></span>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item v-if="userid!==NULL" command>Logout</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <!-- <span v-if="userid===null" style="margin-left: auto; font-family: Arial;">Please login {{ userid }}</span>
+      <span v-else style="margin-left: auto; font-family: Arial;">User ID: {{ userid }}</span> -->
       <button class="view-records-btn" @click="jump(userid,'mylog')">View Borrowing Records</button>
       <button class="view-records-btn" @click="jump(userid,'reservation')">View Reservation Records</button>
       <button class="view-records-btn" @click="jump(userid,'finerecord')">View Fine Records</button>
@@ -36,7 +44,7 @@
 export default {
   data(){
     return {
-      userid: null
+      userid: NULL
     }
   },
   mounted() {
@@ -52,6 +60,22 @@ export default {
       this.$router.push({
         path:"/"+userid+"/"+to
       })
+    },
+    handleCommand() {
+        this.$toast.info("Please log in again.",{
+            position: "top-center",
+            timeout: 1200,
+            closeOnClick: true,
+            pauseOnFocusLoss: false,
+            pauseOnHover: false,
+            draggable: false,
+            draggablePercent: 0.6,
+            showCloseButtonOnHover: false,
+            hideProgressBar: true,
+            closeButton: "button",
+            icon: true,
+            rtl: false
+        });
     },
   }
 }
